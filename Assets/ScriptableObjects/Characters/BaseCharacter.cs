17993ce09eb83b2c7ba31ef7df1060e1d77d917f
Wolfleader <james.morgan@iinet.net.au>
2021-11-05 11:06:07 +0800

    fixed git ignore

---
+++ Assets/ScriptableObjects/Characters/BaseCharacter.cs
@@ -0,0 +1,19 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace ScriptableObjects.Characters
+{
+    [CreateAssetMenu(fileName = "Character", menuName = "Character")]
+    public class BaseCharacter : ScriptableObject
+    {
+        struct Stats
+        {
+            [ShowInInspector]
+            private float _startingHealth;
+           private float _startingStamina;
+        }
+        
+       [ShowInInspector] 
+       private Stats stats;
+    }
+}
