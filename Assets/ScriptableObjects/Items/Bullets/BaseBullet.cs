c448ba86acd4688f1903147a8a175efd764d1878
Wolfleader <james.morgan@iinet.net.au>
2021-11-06 15:10:03 +0800

    working on weapon and bullets

---
+++ Assets/ScriptableObjects/Items/Bullets/BaseBullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.Items.Bullets
+{
+    [CreateAssetMenu(fileName = "Bullet", menuName = "Items/Bullet")]
+    public class BaseBullet : BaseItem
+    {
+        [SerializeField] private float speed = 100f;
+        public float Speed => speed;
+        
+        [SerializeField]
+        private int damage =  15;
+        public int Damage => damage;
+
+        [SerializeField] private float destroyTime = 8f;
+        public float DestroyTime => destroyTime;
+    
+        [SerializeField] private float casingDestroyTime = 4f;
+        public float CasingDestroyTime => casingDestroyTime;
+        
+        [SerializeField] private GameObject bulletPrefab;
+        public GameObject BulletPrefab => bulletPrefab;
+
+        [SerializeField] private GameObject bulletCasingPrefab; 
+        public GameObject BulletCasingPrefab => bulletCasingPrefab;
+    }
+}
---
+++ Assets/ScriptableObjects/Items/Weapons/BaseWeapon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Internal;
+
+namespace ScriptableObjects.Items.Weapons
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Items/Weapon")]
+    public class BaseWeapon : BaseItem
+    {
+        [SerializeField] private int ammoCapacity = 30;
+        public int AmmoCapacity => ammoCapacity;
+
+        [SerializeField] private int bulletsToFire = 1;
+        public int BulletsToFire => bulletsToFire;
+    
+        [SerializeField] private float fireRate = 0.1f; // fire rate in seconds. (how often to shoot a bullet)
+        public float FireRate => fireRate;
+
+        [SerializeField] private float reloadTime = 2.3f; // reload time in seconds
+        public float ReloadTime => reloadTime;
+
+        [SerializeField] private bool isHitScan = true;
+        public bool IsHitScan => isHitScan;
+        
+        // shoot animation?
+        // reload animation?
+    }
+}
